5928fab181907550174e74e92bb4a8ff54935717
borisonekenobi <borisonekenobi@gmail.com>
2022-10-10 11:22:29 -0400

    Will now check for bad input

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,58 @@ public class Main {
 	private static Board board;
 
 	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		byte length;
+		byte width;
+		byte numMines;
+
 		System.out.println("Welcome to Mine Sweeper!");
 
-		Scanner input = new Scanner(System.in);
-		byte length = 16;
-		byte width = 16;
-		/*do {
+		do {
+			try {
+				System.out.print("Choose the length and width of your board: ");
+				length = input.nextByte();
+				width = input.nextByte();
+			} catch (Throwable err) {
+				System.out.println("Incorrect Input!");
+				length = 0;
+				width = 0;
+				input.next();
+			}
+		} while (length <= 0 && width <= 0);
 
-		} while (length == null && width == null);*/
-
-		byte numMines = 30;
-		/*do {
-
-		} while (numMines == null);*/
+		do {
+			try {
+				System.out.print("Choose the amount of mines on your board: ");
+				numMines = input.nextByte();
+			} catch (Throwable err) {
+				System.out.println("Incorrect Input!");
+				numMines = 0;
+				input.next();
+			}
+		} while (numMines <= 0);
 
 		System.out.println("Creating board!");
 		board = new Board(length, width, numMines);
 		outputBoard();
 		while (true) {
 			System.out.print("Choose space to reveal: ");
-			byte[] position = {(byte) (input.nextByte() - 1), (byte) (input.nextByte() - 1)};
+			byte[] position;
+			try {
+				position = new byte[]{(byte) (input.nextByte() - 1), (byte) (input.nextByte() - 1)};
+			} catch (Throwable err) {
+				System.out.println("Incorrect Input!");
+				input.next();
+				continue;
+			}
 			board.updateBoard(position);
 			if (board.lostGame(position)) {
 				endGame("You hit a mine!", position);
+				break;
 			}
 			if (board.wonGame()) {
 				endGame("You survived!", position);
+				break;
 			}
 			outputBoard();
 		}
@@ -66,6 +92,5 @@ public class Main {
 		board.showMines(pos);
 		outputBoard();
 		System.out.println(endMessage);
-		System.exit(0);
 	}
 }
